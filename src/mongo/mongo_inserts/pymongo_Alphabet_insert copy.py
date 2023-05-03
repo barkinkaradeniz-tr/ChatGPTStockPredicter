169f2a8885197675f12d63077dd906dade449d34
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2023-05-03 14:27:01 +0200

    Create the required scripts for fetching and inserting news to MongoDB

---
+++ src/mongo/mongo_inserts/pymongo_Alphabet_insert copy.py
@@ -0,0 +1,50 @@
+import requests
+from datetime import datetime  
+from datetime import timedelta  
+from src.mongo.pymongo_get_database import get_database
+
+
+dbname = get_database()
+collection_name = dbname["AlphabetNews"]
+
+dateObj = datetime(2021, 1, 3)
+
+while dateObj != datetime(2023, 4, 2):
+  dateString = dateObj.strftime("%Y-%m-%d")
+
+  url = ('https://eodhistoricaldata.com/api/news?'
+        's=GOOG&'
+        'language=en&'
+        'from={}&'
+        'to={}&'
+        'api_token=644bb53acdefe0.46250516').format(dateString, dateString)
+
+  response = requests.get(url)
+
+  for i in range(len(response.json())):
+    item = {
+      "date" : response.json()[i]["date"],
+      "content" : response.json()[i]["content"]
+    }
+
+    collection_name.insert_one(item)
+
+  dateObj = dateObj + timedelta(days=1)
+
+# item_1 = {
+#   "item_name" : "Blender",
+#   "max_discount" : "10%",
+#   "batch_number" : "RR450020FRG",
+#   "price" : 340,
+#   "category" : "kitchen appliance"
+# }
+
+# item_2 = {
+#   "item_name" : "Egg",
+#   "category" : "food",
+#   "quantity" : 12,
+#   "price" : 36,
+#   "item_description" : "brown country eggs"
+# }
+
+# collection_name.insert_many([item_1,item_2])
---
+++ src/mongo/mongo_inserts/pymongo_Microsoft_insert.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime  
+from datetime import timedelta  
+from src.mongo.pymongo_get_database import get_database
+
+
+dbname = get_database()
+collection_name = dbname["MicrosoftNews"]
+
+dateObj = datetime(2021, 1, 3)
+
+while dateObj != datetime(2023, 4, 2):
+  dateString = dateObj.strftime("%Y-%m-%d")
+
+  url = ('https://eodhistoricaldata.com/api/news?'
+        's=MSFT&'
+        'language=en&'
+        'from={}&'
+        'to={}&'
+        'api_token=644bb53acdefe0.46250516').format(dateString, dateString)
+
+  response = requests.get(url)
+
+  for i in range(len(response.json())):
+    item = {
+      "date" : response.json()[i]["date"],
+      "content" : response.json()[i]["content"]
+    }
+
+    collection_name.insert_one(item)
+
+  dateObj = dateObj + timedelta(days=1)
